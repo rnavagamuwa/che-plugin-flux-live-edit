136677aefdbb97f618eca84b26d01fc81cac0ba7
rnavagamuwa <randikanavagamuwa@gmail.com>
2016-08-10 21:31:02 +0530

    new module added for flux

---
+++ plugins/plugin-pair-programming/che-plugin-flux-pair-programming/src/main/java/org/eclipse/che/ide/flux/liveedit/EventActivityHandler.java
@@ -0,0 +1,7 @@
+package org.eclipse.che.ide.flux.liveedit;
+
+/**
+ * Created by rnavagamuwa on 8/10/16.
+ */
+public class EventActivityHandler {
+}
---
+++ plugins/plugin-pair-programming/che-plugin-pair-programming-flux/src/main/java/org/eclipse/che/ide/pairprogramming/flux/FluxExtension.java
@@ -0,0 +1,7 @@
+package org.eclipse.che.ide.pairprogramming.flux;
+
+/**
+ * Created by rnavagamuwa on 8/9/16.
+ */
+public class FluxExtension {
+}
